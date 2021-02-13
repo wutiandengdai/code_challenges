63ff92aff0007cfbf59f444413033bc14e0934bf
wutiandengdai <wutiandengdai@gmail.com>
2021-02-13 18:12:22 +0800

    Solve shortest-path challenge of home page.

--- Path2.0/runner.js
+++ Path2.0/runner.js
@@ -1,8 +1,8 @@
 const directions = {
     UP: 'UP',
+    DOWN: 'DOWN',
     LEFT: 'LEFT',
-    RIGHT: 'RIGHT',
-    DOWN: 'DOWN'
+    RIGHT: 'RIGHT'
 }
 
 const colValues = {
@@ -13,201 +13,224 @@ const colValues = {
 }
 
 //start and exit
+var noEntry = false;
+var exitReached = false;
+var noExit = false;
 var startRow = 0;
 var startCol = 0;
-var exitRow = 3;
 var exitCol = 0;
+var exitRow = 0;
+var stepCount = 0;
+
 
 //solve function for challege 1- default challenge
+//Find the shortest path from start to exit of the grid.
 function solve(){
     var grid = cGrid;
-    var curRow = 0;
-    var curCol = 0;
-    var prevRow = 0;
-    var prevCol = 0;
-    var stepCount = 0;
-    var exitReached = false;
-    var noexit = false;
+    var point = {
+        row: 0,
+        col: 0
+    };
+    stepCount = 0;  //resetStep
+    exitRow = grid.length-1;
     var minDistance = -1;
-    var nextDirection;
-    var moveUp = {};
-    var moveLeft = {};
-    var moveRight = {};
-    var moveDown = {};
-    
 
-    //Mark the start on UI
-    grid[startRow][startCol] = colValues.PATH;
-    var elementID = `${startRow}:${startCol}`;
-    document.getElementById(elementID).setAttribute("blockValue", "step");
-
-    //solve
+    //2. Walk through grid, loop each step
     do{
-        let nextStep = [];
-        let prevRow = curRow;
-        let prevCol = curCol;
+        let nextSteps = [];
+        let step = {};
 
-        moveUp = move(curRow, curCol, grid, directions.UP);
-        if (moveUp.canMove){
-            nextStep.push(moveUp);
+        for( var direct in directions){
+            step = movable(point, grid, direct);
+            if(step.canMove){
+                step.direction = direct;
+                nextSteps.push(step);
+            }
         }
-        moveDown = move(curRow, curCol, grid, directions.DOWN);
-        if (moveDown.canMove){
-            nextStep.push(moveDown);
-        }
-        moveLeft = move(curRow, curCol, grid, directions.LEFT);
-        if (moveLeft.canMove){
-            nextStep.push(moveLeft);
-        }
-        moveRight = move(curRow, curCol, grid, directions.RIGHT);
-        if (moveRight.canMove){
-            nextStep.push(moveRight);
-        }
-
+        
         //If no direction walkable, exit
-        if(nextStep.length == 0){
+        if(nextSteps.length == 0){
             noExit = true;
             break;
         }
 
-        //sort distance and take the shortest direction
-        nextStep.sort((a, b) => (a.minDistance - b.minDistance));
-        //Update current position
-        switch(nextStep[0].direction){
-            case directions.UP:
-                stepCount ++;
-                curRow += 1;
-                break;
-            case directions.DOWN:
-                stepCount ++;
-                curRow -= 1;
-                break;
-            case directions.LEFT:
-                stepCount ++;
-                curCol -= 1;
-                break;
-            case directions.RIGHT:
-                stepCount ++;
-                curCol += 1;
-                break;
+        //3. sort distance and take the shortest direction
+        nextSteps.sort((a, b) => (a.minDistance - b.minDistance));
+
+        //mark current step and make the move
+        point = markElement(point, grid, nextSteps);
+
+        //5. test exit condition
+        if (point.row == exitRow && point.col == exitCol){
+            exitReached = true;
+            grid[exitRow][exitCol] = colValues.PATH;
+            document.getElementById(`${exitRow}:${exitCol}`).setAttribute("blockValue", "step");
+            stepCount ++;
+            break;
         }
-        //mark element
-        exitReached = markElements(curRow, curCol, prevRow, prevCol, grid);
+    } while(true);
 
-    } while(exitReached == false || noExit == true);
-
-    if(exitReached == true){
-        document.getElementById("results").innerHTML = `Success! It took ${stepCount} step(s)`;
-    }else {
-        document.getElementById("results").innerHTML = "Cannot find an exit!"
-    }
+    writeResults();
 }
 
 
 //Solve challenge 2.0 - grid 3,4,5
-function runnerSolve(){
+//Maximum one path available, no start and exit point
+//No distance sort, start col and exit col won't be used.
+function solvePath(){
+    var grid = cGrid;
+    const startRow = 0;
+    const exitRow = grid.length;
+    var curRow = 0;
+    var curCol = 0;
+    var prevRow = 0;
+    var prevCol = 0;
 
+    //1. Loop start row , find the start col and mark
+    startRow = 0; 
+    exitRow = grid.length -1;
+    for ( ;curCol < grid[startRow].length; curCol++){
+        noEntry = true;
+        if (grid[startRow][curCol] == colValues.OPEN){
+            curRow = startRow;
+            noEntry = false;
+            break;
+        }
+    }
+    if(noEntry){
+        writeResults();
+        return false;
+    }
+    
+
+    //2. loop and find the path
+    
+
+    //3. mark path
+
+    //4. Test exit condition
 } 
 
 
-//move action
-function move(curRow, curCol, grid, direction){
-    var targetRow = curRow;
-    var targetCol = curCol;
-    var targetVal = "";
+//Test if movable
+function movable(point, grid, direction){
     var canMove = false;
     var minDistance = -1;
 
-    //move
-    switch (direction){
-        case directions.UP:
-            targetRow += 1;
-            break;
-        case directions.LEFT:
-            targetCol -= 1;
-            break;
-        case directions.RIGHT:
-            targetCol += 1;
-            break;
-        case directions.DOWN:
-            targetRow -= 1;
-            break;
-    }
+    //get target point
+    var tPoint = getNextPoint(point, direction);
 
     //Check bounds
-    if (targetRow > grid.length -1 || targetRow < 0 || targetCol > grid[targetRow].length || targetCol < 0){
+    if (tPoint.row > grid.length -1 || tPoint.row < 0 || tPoint.col > grid[point.row].length || tPoint.col < 0){
         return {
             canMove : false,
             minDistance :-1,
-            direction : direction,
-            colValue: colValues.WALL
+            colValue : colValues.DEADEND
         };
     }
 
     //Get the value of the square
-    targetVal = grid[targetRow][targetCol];
-
-    //Check if current position is start
-    if (targetRow == startRow && targetCol == startCol){
-        return {
-            canMove : false,
-            minDistance : -1,
-            direction: direction,
-            colValue : colValues.WALL
-        };
-    
-    //cannot move to wall or deadend
-    } else if (targetVal == colValues.WALL || targetVal == colValues.DEADEND){
-        return {
-            canMove : false,
-            minDistance : -1,
-            direction: direction,
-            colValue : targetVal
-        };
-
-    // can move if path
-    } else if (targetVal == colValues.PATH || targetVal == colValues.OPEN){
-        return {
-            canMove : true,
-            minDistance : getMinDistance(targetRow, targetCol),
-            direction: direction,
-            colValue: targetVal
-        };
+    var type = grid[tPoint.row][tPoint.col];
+    switch(type){
+        case colValues.OPEN:
+        case colValues.PATH:
+        case colValues.BRANCH:
+            canMove = true;
+            minDistance = getMinDistance(tPoint);
+            break;
     }
-
     return {
-        canMove: false,
-        minDistance: -1,
-        direction: direction,
-        colValue: colValues.WALL
-    };
-
+                canMove: canMove,
+                minDistance: minDistance,
+                colValue : type
+            };
 }
+
+
+// if the target has been visted before, mark previous brick as deadend and update grid
+// else mark as passed (step) and update grid
+function markElement(point, grid, nextSteps){
+    var curRow = point.row;
+    var curCol = point.col;
+    var elementID = `${curRow}:${curCol}`;
+    var nextStep = nextSteps[0];
+
+    //If there is only one path, mark current brick as deadend, move to the next step 
+    if (nextSteps.length == 1){
+        grid[curRow][curCol] = colValues.DEADEND;
+        document.getElementById(elementID).setAttribute("blockValue", "deadend");
+        stepCount ++;
+
+    //If there is only one shortest path, mark current as stepped and move to the next step
+    } else if (nextStep.minDistance < nextSteps[1].minDistance){
+        grid[curRow][curCol] = colValues.PATH;
+        document.getElementById(elementID).setAttribute("blockValue", "step");
+        stepCount ++;
+    //if there is more than one shortest path, mark current as branch 
+    //and move to the next unstepped step
+    } else{
+        grid[curRow][curCol] = colValues.PATH
+        document.getElementById(elementID).setAttribute("blockValue", "step");
+         //If path walked, take another one
+        var curMinDis = getMinDistance(point);
+        //if nextStep is closer, take the untaken path;
+        //if nextStep is father, take the step taken
+        if (curMinDis > nextStep.minDistance){
+            if(nextStep.colValue != colValues.OPEN){
+                nextStep = nextSteps[1];
+                stepCount ++;
+            }
+        } else {
+            //Minus step towards wrong brick and step returning
+            if(nextStep.colValue == colValues.OPEN){
+                nextStep = nextSteps[1];
+                stepCount -= 2;
+            }
+        }
+        
+    }
+    return getNextPoint(point, nextStep.direction);
+}
+
 
 //Get the minDistance
-function getMinDistance (targetRow, targetCol){
-    return Math.abs(exitRow-targetRow) + Math.abs(exitCol-targetCol);
+function getMinDistance (point){
+    return Math.abs(exitRow-point.row) + Math.abs(exitCol-point.col);
+}
+
+//get next move point
+function getNextPoint(point, direction){
+    let tPoint = {
+                row: point.row,
+                col: point.col
+            };
+    switch(direction){
+        case directions.UP:
+            tPoint.row += 1;
+            break;
+        case directions.DOWN:
+            tPoint.row -= 1;
+            break;
+        case directions.LEFT:
+            tPoint.col -= 1;
+            break;
+        case directions.RIGHT:
+            tPoint.col += 1;
+            break;
+    }
+    return tPoint;
 }
 
 
-// if the target has been visted before, mark as deadend
-function markElements(targetRow, targetCol, prevRow, prevCol, grid){
-
-    var elementID = "";
-    if (grid[targetRow][targetCol] == colValues.PATH){
-        grid[prevRow][prevCol] = colValues.DEADEND;
-        elementID = `${prevRow}:${prevCol}`;
-        document.getElementById(elementID).setAttribute("blockValue", "deadend");
+//write results after solve 
+function writeResults(){
+    var result = "";
+    if(noEntry){
+        result = "Can not find an entry!";
+    }else if(exitReached){
+        result = `Success! It took ${stepCount} step(s)`;
+    }else {
+        result = "Cannot find an exit!";
     }
-
-    elementID = `${targetRow}:${targetCol}`;
-    document.getElementById(elementID).setAttribute("blockValue", "step");
-    grid[targetRow][targetCol] = colValues.PATH;
-
-    //test exit
-    if (targetRow == exitRow && targetCol == exitCol){
-        return true;
-    } else {
-        return false;
-    }
+    document.getElementById("results").innerHTML = result;
 }
--- README.md
+++ README.md
@@ -5,4 +5,12 @@
 
 
 ## Path2.0 - A Code Chanllenge
-1. Debug with 
+1. Get the shortest Path
+	Know entry and exit, get the shortest path, return steps count.
+	- Store current point
+	- try on every direction and test if movable, mark as wall(unmovable), deadend(unmovable), passed, open
+	- store all movable directions and sort
+	- Not going back - if there is only one path, mark current as deadend and move to the next
+	- Take shortest path - if there is more than one path, take the shortest path
+	- Take the untaken path if closer - If there are more then one shortesst path, take the untaken path if both paths are closer than current point.
+	- Take the old path if father - If there are more than one shortest path, take the path taken if both paths are farther than current point.
